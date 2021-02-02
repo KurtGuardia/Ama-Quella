e5b9f1bfd3b2ee256f87b0c04a148c646bcaeab6
Kurt Guardia <kurtguardia@gmail.com>
2021-02-02 11:55:37 +0100

    Icons section done, non responsive

--- app/src/views/tutorial/Tutorial.js
+++ app/src/views/tutorial/Tutorial.js
@@ -6,7 +6,7 @@ const Tutorial = () => {
   return (
     <div className='tutorial'>
       <div className='tutorial__top'>
-        <div className='tutorial__top--number one'>
+        <div className='tutorial__top--number one dark'>
           <div className='square'></div>
           <div className='numb'>1</div>
         </div>
@@ -28,30 +28,35 @@ const Tutorial = () => {
         </div>
       </div>
       <div className='tutorial__icons'>
-        <div className='tutorial__icons--icon one down'>
+        <div className='tutorial__icons--icon one down dark'>
           <svg>
             <use href={sprite + '#user-icon'} />
           </svg>
+          <p>Inicia sesión</p>
         </div>
         <div className='tutorial__icons--icon two'>
           <svg>
             <use href={sprite + '#declare'} />
           </svg>
+          <p>Ingresa a Declarar</p>
         </div>
         <div className='tutorial__icons--icon three down'>
           <svg>
             <use href={sprite + '#coins'} />
           </svg>
+          <p>Selecciona Ingreso o Gasto</p>
         </div>
         <div className='tutorial__icons--icon four'>
           <svg>
             <use href={sprite + '#fill'} />
           </svg>
+          <p>Rellena el formulario</p>
         </div>
         <div className='tutorial__icons--icon five down'>
           <svg>
             <use href={sprite + '#done'} />
           </svg>
+          <p>Pulsa Guardar y continúa con otra factura</p>
         </div>
       </div>
       <div className='tutorial__buttons'>
