6d71e000260dee201a6180f3556957b56f1b8720
oliver <olivercastroteran@gmail.com>
2021-02-08 13:31:17 -0400

    route guardering implemented plus spinner on auth actions added

--- app/src/components/UI/index.js
+++ app/src/components/UI/index.js
@@ -1,1 +1,2 @@
 export { default as Btn } from './btn/Btn';
+export { default as Spinner } from './spinner/Spinner';
--- app/src/views/home/Home.js
+++ app/src/views/home/Home.js
@@ -1,6 +1,17 @@
 import './Home.scss';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Home = () => {
+  const uid = useSelector((state) => state.firebase.auth.uid);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!uid) history.push('/auth');
+    // eslint-disable-next-line
+  }, [uid]);
+
   return (
     <div className="home">
       <h1>Home</h1>
